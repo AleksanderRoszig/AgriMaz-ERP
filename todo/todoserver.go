c93e4358cc26f03b7d1f8ad510c70cedeff29d6f
aleksrosz <aleksrosz@gmail.com>
2021-03-19 22:19:44 +0100

    Function ensureTableExists

--- todo/todoserver.go
+++ todo/todoserver.go
@@ -1,22 +1,28 @@
 package main
+
 import (
 	"database/sql"
-	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 	log "github.com/sirupsen/logrus"
 	"io"
 	"net/http"
-	//"database/sql"
 )
+var db *sql.DB
+const(
+	user= "myuser"
+	password = "palakopowe"
+	host = "35.157.134.249"
+	port = 5432
+)
+const createDatabase = `CREATE DATABASE tests;`
 
 const tableCreationQuery = `CREATE TABLE IF NOT EXISTS todolist
 (
-    
-    name TEXT NOT NULL,
-    price NUMERIC(10,2) NOT NULL DEFAULT 0.00,
-    CONSTRAINT products_pkey PRIMARY KEY (id)
+    id SERIAL PRIMARY KEY, 
+    description varchar(40) NOT NULL,
+    completed integer NOT NULL
 )`
 
 type TodoItemModel struct{
@@ -24,39 +30,31 @@ type TodoItemModel struct{
        Description string
        Completed bool
 }
+func ensureTableExists() {
+	if _, err := db.Exec(createDatabase);
+	err != nil {
+		log.Fatal(err)
+	}
+
+	if _, err := db.Exec(tableCreationQuery);
+	err != nil {
+		log.Fatal(err)
+	}
+}
 
 func CreateItem(w http.ResponseWriter, r *http.Request) {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
-	db, err := sql.Open("postgres", psqlInfo)
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Successfully connected!")
-
-
     description := r.FormValue("description")
     log.WithFields(log.Fields{"description": description}).Info("Add new TodoItem. Saving to database.")
     todo := &TodoItemModel{Description: description, Completed: false}
 	var lastInsertId int
-	err = db.QueryRow("INSERT INTO todolist(name,date,price) VALUES($1,$2) returning id", "car", "19.03.2021", "2332").Scan(&lastInsertId)
+	var err error
+	err = db.QueryRow("INSERT INTO todolist(description, completed) VALUES($2, $3) returning id", todo).Scan(&lastInsertId)
 	if err != nil {
 		log.Fatal("Failed to execute query: ", err)
 	}
 	userSql := "SELECT * FROM todolist"
 	err = db.QueryRow(userSql).Scan(&todo.Id, &todo.Description, &todo.Completed)
-
-    //result := db.Last(&todo)
     w.Header().Set("Content-Type", "application/json")
-    json.NewEncoder(w).Encode(result.Value)
 }
 
 
@@ -70,10 +68,30 @@ func init() {
 	log.SetFormatter(&log.TextFormatter{})
 	log.SetReportCaller(true)
 }
+func connectToDB() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s sslmode=disable",
+		host, port, user, password)
+	var err error
+	db, err = sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected!")
+}
+
 
 func main() {
 	log.Info("Starting Todolist API server")
+	connectToDB()
+    ensureTableExists()
 	router := mux.NewRouter()
 	router.HandleFunc("/healthz", Healthz).Methods("GET")
 	http.ListenAndServe(":8000", router)
+
 }
